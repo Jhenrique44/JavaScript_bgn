fbc54d9bdbaf3b65afcc93c2ff734acada2da580
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-03-17 14:53:13 -0300

    1

---
+++ scripts.js
@@ -0,0 +1,28 @@
+//JavaScript it is case sensitive
+ 
+console.log('ola java')
+
+console.log(`valor ${1 + 1}`) 
+
+console.log(false)
+
+// objeto 
+console.log({
+    name: 'jojo',
+    idade: 20,
+    andar: function(){
+        console.log('andar')
+    }
+
+})
+
+//lista
+
+console.log([
+
+    "leite",
+    "ovos",
+    "arroz",
+    "queijo",
+])
+
