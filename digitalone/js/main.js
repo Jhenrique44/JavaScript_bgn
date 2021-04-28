6eab0454a4119adf1c12028ef77329b59a512e7a
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-04-28 11:31:09 -0300

    JSdigitalone

---
+++ digitalone/js/main.js
@@ -0,0 +1,120 @@
+function load(){
+    alert("Pagina carregada")
+}
+function change(elemento){
+    console.log(elemento.value);
+}
+
+
+/*function clicou(){
+    
+    document.getElementById("agradecimento").innerHTML ="<b>Obrigado por clicar</b>";
+    // console.log(document.getElementById("agradecimento"););
+};
+function redirecionar(){
+    window.open("https://www.youtube.com/watch?v=ERD4CbBDNI0");
+    //abre na mesma pagina
+    //window.location.href = "https://www.youtube.com/watch?v=ERD4CbBDNI0";
+}
+function trocar(elemento){
+
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado"
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse"
+}
+*/
+//#Functions
+// function soma(n, n1){
+
+//     return n + n1;
+
+
+// }
+// function validdaIdade(idade){
+
+//     var validar;
+//     if(idade >= 18){
+//         validar =  true;
+//     }
+//     else{
+//         validar  = false
+//     }
+// }
+
+// var idade =  prompt("Qual sua idade?")
+// console.log(validdaIdade(idade));
+
+
+
+// var d =  new Date();
+// alert(d.getMonth()+1);
+// alert(d.getTime);
+
+// var count = 0;
+
+// for (count = 0; count < 5; count++) {
+    
+//     alert(count);
+    
+// };
+// while (count < 5) {
+//     console.log(count);
+//     count++;
+// };
+
+
+// var age = prompt("qual sua idade?");
+
+// if(age >= 18){
+//     alert("maior de idade");
+
+// }
+// else{
+//     alert("menor idade");
+// }
+
+
+/*
+var fruta = [{nome: "maça", cor:"red"}, {nome: "uva", cor: "purple"}]
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+// var fruta = {nome: "maça", cor:"red"}
+// console.log(fruta.nome);
+// alert(fruta.cor);
+
+
+
+//##list!
+//var list = ["maça","melancia", "laranja"];
+
+//list.push("uva");
+//lista.push();
+
+//console.log(list[1]);
+//console.log(list.reverse);
+//console.log(list.length());
+
+//pegando a ultima letra
+//console.log(list.toString()[0]);
+//console.log(list.join(" - "));
+
+
+/*
+var name = "Jojo";
+var age = 20;
+var frase = "";
+
+alert(name + "has" + age + "years");
+
+console.log(name)
+console.log(frase.replace("", ""));
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+
+alert(frase.replace("", ""))
+*/
